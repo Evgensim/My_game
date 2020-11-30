f66159e19615c41b447e5390a0fe70d6d13683eb
Evgeny <zhenya.2015aa@gmail.com>
2020-11-30 12:04:26 +0300

    Second commit
    My first commit.

---
+++ Assets/Scripts/Weapon/GunPickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunPickup : MonoBehaviour
+{
+    public GameObject ourPistol;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //gunPickup.Play();    
+        ourPistol.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
